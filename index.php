d573fcdbda2c861ab1928db634a1ba8c588a085e
Toxa2015Ars <tokha.kovalenko@inbox.ru>
2022-05-27 14:51:37 +1000

    Load

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<head>
+    <meta name="content-type" content="text/html; charset=utf-8">
+    <meta name="keywords" content="Umans - Внедренческий центр">
+    <meta name="description" content="Umans - Внедренческий центр">
+    <title>Umans - Внедренческий центр</title>
+    <link href="css/bootstrap.css" type="text/css"  rel="stylesheet">
+    <link href="css/style.css" type="text/css" rel="stylesheet">
+    <script src="/js/bootstrap.bundle.js"></script>
+    <script src="https://kit.fontawesome.com/cd99dd88b4.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <section class="top">
+        <div class="container">
+            <div class="row">
+                <div class="d-flex align-items-center">
+                    <div class="col-4 mb-3">
+                        <a href="/"><img src="images/logo.png"></a>
+                    </div>
+                    <div class="col-6 top-contacts">
+                        <span class="top-contacts-item"><i class="fa-solid fa-phone me-1"></i>+7 (423) 240-00-40</span>
+                        <span class="top-contacts-item ms-3"><i class="fa-solid fa-location-dot me-1"></i>г. Владивосток, Некрасовская, 48 А</span>
+                    </div>
+                    <div class="col-2 mt-3 top-callback">
+                        <a class="btn-red mb-2">Заказать звонок</a>
+                        <a class="btn-red mb-2 wa" style="font-size:8pt">Написать в WhatsApp</a>
+                    </div>
+                </div>
+            </div>
+            <div class="row" style="margin-top: 10rem">
+                <div id="topSlider" class="carousel slide mb-5" data-bs-ride="carousel">
+                    <div class="container"><div class="row">
+                        <div class="col-1 text-end me-0">
+                            <button class="carousel-control-prev top-slider-control" type="button" data-bs-target="#topSlider"  data-bs-slide="prev">
+                                <i class="fa-solid fa-chevron-left fa-5x"></i>
+                                <span class="visually-hidden">Предыдущий</span>
+                            </button>
+                        </div>
+                        <div class="col-10">
+                            <div class="carousel-inner">
+                                <div class="carousel-item text-center active">
+                                    <h1 class="topSlider-h">Скидка 50%  малому и среднему бизнесу на облачные программы "1С"</h1>
+                                    <div class="container"><div class="row">
+                                        <div class="col-8"><a class="btn-red" href="http://1c.cloud/?utm_source=1c.ru&utm_medium=main&utm_campaign=banner" target="_blank">Программа поддержки МСП от Минцифры России и РФРИТ</a></div>
+                                        <div class="col-4"><a class="btn-tran"><i class="fa-solid fa-phone-volume me-2"></i>Консультация специалиста</a></div>
+                                    </div></div>
+                                </div>
+                                <div class="carousel-item text-center">
+                                    <h1 class="topSlider-h">Продление акции "Новый бизнес начни с 1С!" до конца 2022 года</h1>
+                                    <div class="container"><div class="row">
+                                        <div class="col-8"><a class="btn-red" href="http://v8.1c.ru/action3000/" target="_blank">"1С:Бухгалтерия 8" за 3 000 рублей с бесплатной бессрочной поддержкой и годовыми дополнительными сервисами</a></div>
+                                        <div class="col-4"><a class="btn-tran"><span class="middle"><i class="fa-solid fa-phone-volume me-2"></i>Консультация специалиста</span></a></div>
+                                    </div></div>
+                                </div>
+                                <div class="carousel-item text-center">
+                                    <h1 class="topSlider-h">1С: Предприятие 8 через Интернет</h1>
+                                    <div class="container"><div class="row">
+                                        <div class="col-8"><a class="btn-red" href="http://alterlogo.1cfresh.com/" target="_blank">там где вам удобно. Для работы нужен только Интернет. И никаких забот по обслуживанию!</a></div>
+                                        <div class="col-4"><a class="btn-tran"><span class="middle"><i class="fa-solid fa-phone-volume me-2"></i>Консультация специалиста</span></a></div>
+                                    </div></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-1">
+                            <button class="carousel-control-next top-slider-control" type="button" data-bs-target="#topSlider"  data-bs-slide="next">
+                                <i class="fa-solid fa-chevron-right fa-5x"></i>
+                                <span class="visually-hidden">Следующий</span>
+                            </button>
+                        </div>
+                    </div></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="generic">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid d-flex"><div class="collapse navbar-collapse justify-content-center" id="navbarNav"><ul class="navbar-nav top-navbar">
+                <li class="nav-item me-5"><a class="nav-link" aria-current="page" href="#service"><i class="fa-solid fa-layer-group fa-2x me-3"></i><span class="top-navbar-item">Услуги</span></a></li>
+                <li class="nav-item me-5"><a class="nav-link" aria-current="page" href="#action"><i class="fa-solid fa-ticket fa-2x me-3"></i><span class="top-navbar-item">Акции</span></a></li>
+                <li class="nav-item me-5"><a class="nav-link" aria-current="page" href="#portfolio"><i class="fa-solid fa-briefcase fa-2x me-3"></i><span class="top-navbar-item">Портфолио</span></a></li>
+                <li class="nav-item me-5"><a class="nav-link" aria-current="page" href="#about"><i class="fa-solid fa-pager fa-2x me-3"></i><span class="top-navbar-item">О компании</span></a></li>
+                <li class="nav-item me-5"><a class="nav-link" aria-current="page" href="#contact"><i class="fa-solid fa-address-book fa-2x me-3"></i><span class="top-navbar-item">Контакты</span></a></li>
+            </ul></div></div>
+        </nav>
+        <div class="container">
+            <div class="row">
+                <ul class="nav nav-tabs mt-5" id="tabsBuisness" role="tablist">
+                    <li class="nav-item tabs-h" role="presentation">
+                        <h3>КАК СДЕЛАТЬ ВАШ БИЗНЕС ЭФФЕКТИВНЕЕ</h3>
+                    </li>
+                    <li class="nav-item tabs-button" role="presentation">
+                        <a class="nav-link active" id="firstTab" data-bs-toggle="tab" data-bs-target="#first" type="button" role="tab" aria-controls="first" aria-selected="true"><i class="fa-solid fa-download fa-2x mb-2"></i><br>Реальная автоматизация</a>
+                    </li>
+                    <li class="nav-item tabs-button" role="presentation">
+                        <a class="nav-link" id="secondTab" data-bs-toggle="tab" data-bs-target="#second" type="button" role="tab" aria-controls="second" aria-selected="false"><i class="fa-solid fa-check-to-slot fa-2x mb-2"></i><br>Сопровождение</a>
+                    </li>
+                    <li class="nav-item tabs-button" role="presentation">
+                        <a class="nav-link" id="thirdTab" data-bs-toggle="tab" data-bs-target="#third" type="button" role="tab" aria-controls="third" aria-selected="false"><i class="fa-solid fa-cart-shopping fa-2x mb-2"></i><br>Продажа ПО</a>
+                    </li>
+                    <li class="nav-item tabs-button" role="presentation">
+                        <a class="nav-link" id="fourthTab" data-bs-toggle="tab" data-bs-target="#fourth" type="button" role="tab" aria-controls="fourth" aria-selected="false"><i class="fa-solid fa-id-card fa-2x mb-2"></i><br>Карьера в компании</a>
+                    </li>
+                </ul>
+                <div class="tab-content mb-5" id="tabsBuisnessContent">
+                    <div class="tab-pane show active" id="first" role="tabpanel" aria-labelledby="firstTab">
+                        <div class="container ps-0"><div class="row">
+                            <div class="col-3"><img src="/images/tabs_first.jpg"></div>
+                            <div class="col-9"><span>Реальная автоматизация — способ сокращения издержек и получения большей прибыли, это автоматизация потребностей клиента, напрямую связанных с развитием и ростом эффективности его бизнеса. Затраты на реальную автоматизацию окупаются!(По статистике реальная автоматизация окупает себя в первые 3 месяца запуска на предприятии). Компания «Юманс-ВЦ» гарантирует индивидуальный подход к каждому клиенту, обеспечивая его информационной и технической поддержкой.</span><br><br><a class="tabs-link" href="/develop.php">Подробнее</a></div>
+                        </div></div>
+                    </div>
+                    <div class="tab-pane" id="second" role="tabpanel" aria-labelledby="secondTab">
+                        <div class="container ps-0"><div class="row">
+                            <div class="col-3"><img src="/images/tabs_second.jpg"></div>
+                            <div class="col-9"><span>В процессе эксплуатации программных продуктов 1С у пользователей помимо регулярных ежемесячных задач по обновлению и работе с программами 1С могут возникать нештатные и нестандартные задачи, решить которые под силу только квалифицированным специалистам.&nbsp;<br>Компания «Юманс-ВЦ» предоставляет своим клиентам набор услуг по сопровождению продуктов 1С, направленный на минимизацию любых нештатных ситуаций. Выполняемые нами работы позволяют каждому нашему клиенту всегда получать своевременную помощь.</span><br><br><a class="tabs-link" href="/accomp.php">Подробнее</a></div>
+                        </div></div>
+                    </div>
+                    <div class="tab-pane" id="third" role="tabpanel" aria-labelledby="thirdTab">
+                        <div class="container ps-0"><div class="row">
+                            <div class="col-3"><img src="/images/tabs_third.jpg"></div>
+                            <div class="col-9"><span>Компания &laquo;Юманс-ВЦ&raquo; оказывает весь спектр услуг связанных с программными продуктами фирмы «1С», начиная от подбора необходимого клиенту программного продукта 1С, установки и первоначальной настройки, ежемесячного сопровождения внедренного программного продукта и заканчивая доработкой существующего программного продукта под уникальные потребности заказчика. Компания «Юманс-ВЦ» занимается автоматизацией управления и учета на базе программных продуктов «1С».</span><br><br><a class="tabs-link" href="/sale.php">Подробнее</a></div>
+                        </div></div>
+                    </div>
+                    <div class="tab-pane" id="fourth" role="tabpanel" aria-labelledby="fourthTab">
+                        <div class="container ps-0"><div class="row">
+                            <div class="col-3"><img src="/images/tabs_fourth.jpg"></div>
+                            <div class="col-9"><span>Приглашаем в команду профессионалов компании &laquo;Юманс-ВЦ&raquo; интересных и заинтересованных людей. Предлагаем вакансии для уверенных, решительных, и амбициозных сотрудников. Резюме направлять на электронную почту <a href="mailto:umans@umans.ru">umans@umans.ru</a></span><br><br><a class="tabs-link" href="/career.php">Подробнее</a></div>
+                        </div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
